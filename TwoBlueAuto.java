1455d4fe961bb505b5d56d2c8816ebf7b75374a0
mackant2 <41598301+mackant2@users.noreply.github.com>
2021-10-24 13:52:28 -0500

    2020 CodeBlooded files
    These are an old set of CodeBlooded files

---
+++ TwoBlueAuto.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode1920;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name="Two Blue Auto", group ="A Run")
+
+public class TwoBlueAuto extends TwoBaseAuto{
+
+     public TwoBlueAuto(){
+          super.Alliance = true;
+     }
+}
